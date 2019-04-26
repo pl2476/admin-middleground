096dc0b435ccf6497f5ad889e7ba20a6737e1313
Light Peng <light@imanagesystems.com>
2019-04-26 16:07:45 +0800

    update 2.2.0

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ import os from 'os';
 import pageRoutes from './router.config';
 import webpackPlugin from './plugin.config';
 import defaultSettings from '../src/defaultSettings';
+import slash from 'slash2';
 
 const plugins = [
   [
@@ -12,9 +13,6 @@ const plugins = [
       dva: {
         hmr: true,
       },
-      targets: {
-        ie: 11,
-      },
       locale: {
         enable: true, // default false
         default: 'zh-CN', // default zh-CN
@@ -22,6 +20,13 @@ const plugins = [
       },
       dynamicImport: {
         loadingComponent: './components/PageLoading/index',
+        webpackChunkName: true,
+      },
+      pwa: {
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+          importWorkboxFrom: 'local',
+        },
       },
       ...(!process.env.TEST && os.platform() === 'darwin'
         ? {
@@ -29,14 +34,15 @@ const plugins = [
               include: ['dva', 'dva/router', 'dva/saga', 'dva/fetch'],
               exclude: ['@babel/runtime'],
             },
-            hardSource: true,
+            hardSource: false,
           }
         : {}),
     },
   ],
 ];
 
-// judge add ga
+// 针对 preview.pro.ant.design 的 GA 统计代码
+// 业务上不需要这个
 if (process.env.APP_TYPE === 'site') {
   plugins.push([
     'umi-plugin-ga',
@@ -49,11 +55,12 @@ if (process.env.APP_TYPE === 'site') {
 export default {
   // add for transfer to umi
   plugins,
-  targets: {
-    ie: 11,
-  },
   define: {
     APP_TYPE: process.env.APP_TYPE || '',
+  },
+  treeShaking: true,
+  targets: {
+    ie: 11,
   },
   // 路由配置
   routes: pageRoutes,
@@ -90,7 +97,7 @@ export default {
       const match = context.resourcePath.match(/src(.*)/);
       if (match && match[1]) {
         const antdProPath = match[1].replace('.less', '');
-        const arr = antdProPath
+        const arr = slash(antdProPath)
           .split('/')
           .map(a => a.replace(/([A-Z])/g, '-$1'))
           .map(a => a.toLowerCase());
