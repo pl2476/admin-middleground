c507ce90fb600c8f449681122822297712334143
Light <pl2476@126.com>
2019-05-10 15:07:16 +0800

    add temp lib for latest react-big-calendar

---
+++ src/models/schedule.js
@@ -0,0 +1,53 @@
+import queryCustoms from '@/services/customs';
+import { notification } from 'antd';
+
+export default {
+  namespace: 'schedule',
+
+  state: {
+    data: {
+      list: [],
+      pagination: {},
+    },
+  },
+
+  effects: {
+    *list({ payload }, { call, put, select }) {
+      const oldData = yield select(state => state.client.data);
+      const res = yield call(queryCustoms, payload);
+      if (res.code === 200) {
+        let { data } = res;
+        if (payload.pageNumber > 1) {
+          data = [...oldData.list, ...data];
+        }
+        console.log('client', data);
+        yield put({
+          type: 'saveClient',
+          payload: {
+            list: data,
+            pagination: {
+              pageSize: res.pageSize,
+              total: res.totalRecords,
+              current: res.pageNumber,
+            },
+          },
+        });
+      } else {
+        notification.error({
+          message: 'error',
+          description: 'error',
+          onClick: () => {},
+        });
+      }
+    },
+  },
+
+  reducers: {
+    saveClient(state, action) {
+      return {
+        ...state,
+        data: action.payload,
+      };
+    },
+  },
+};
--- src/pages/List/Schedule.js
+++ src/pages/List/Schedule.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import BigCalendar from 'react-big-calendar';
+import BigCalendar from '@/tempLib/dist/react-big-calendar';
 import moment from 'moment';
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
+import withDragAndDrop from '@/tempLib/lib/addons/dragAndDrop';
+import { Row, Col, Calendar, Card } from 'antd';
+import { formatMessage } from 'umi/locale';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import style from './Schedule.less';
 
 const localizer = BigCalendar.momentLocalizer(moment);
 const DragAndDropCalendar = withDragAndDrop(BigCalendar);
-
+console.log(formatMessage);
 const events = [
   {
     id: 0,
@@ -22,7 +25,7 @@ const events = [
     allDay: true,
     start: new Date(2018, 0, 29, 14, 0, 0),
     end: new Date(2018, 0, 29, 16, 30, 0),
-    // resourceId: 2,
+    resourceId: 2,
   },
   {
     id: 2,
@@ -52,11 +55,43 @@ const resourceMap = [
   { resourceId: 4, resourceTitle: 'Meeting room 2' },
 ];
 
+const isType = type => target => Object.prototype.toString.call(target) === `[object ${type}]`;
+console.log(isType('String')('a'));
+function add(a) {
+  let ac = a;
+  function sum(b) {
+    // 使用闭包
+    ac += b; // 累加
+    return sum;
+  }
+  // 重写toString()方法
+  sum.toString = () => ac;
+  return sum; // 返回一个函数
+}
+const arr = [1, 2, 3, [2, [12233, [1, 4, 7]]]];
+const flatArr = target => Array.from(new Set(target.flat(Infinity))).sort((a, b) => a - b);
+console.log(flatArr(arr));
+console.log(add(1)(2), add(1)(2)(4) === 7, typeof add(1)(2)(4), add(1)(2)(4) + 0);
+
+const outData = [
+  {
+    id: 1,
+    name: 'outData1',
+  },
+  {
+    id: 2,
+    name: 'outData2',
+  },
+];
+
+const formatName = (name, count) => `${name} ID ${count}`;
+
 class Schedule extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
       events,
+      draggedEvent: null,
     };
 
     this.moveEvent = this.moveEvent.bind(this);
@@ -114,38 +149,120 @@ class Schedule extends React.PureComponent {
     console.log(this.state);
   };
 
+  onPanelChange = (value, mode) => {
+    console.log(value, mode);
+  };
+
+  handleDragStart = (name, count) => {
+    console.log(name, count);
+    this.setState({ draggedEvent: name });
+  };
+
+  customOnDragOver = event => {
+    const { draggedEvent } = this.state;
+    if (draggedEvent !== 'undroppable') {
+      console.log('preventDefault');
+      event.preventDefault();
+    }
+  };
+
+  onDropFromOutside = ({ start, end, allDay }) => {
+    const { draggedEvent, counters } = this.state;
+    const event = {
+      title: formatName(draggedEvent, counters[draggedEvent]),
+      start,
+      end,
+      isAllDay: allDay,
+    };
+    const updatedCounters = {
+      ...counters,
+      [draggedEvent]: counters[draggedEvent] + 1,
+    };
+    this.setState({ draggedEvent: null, counters: updatedCounters });
+    this.newEvent(event);
+  };
+
   render() {
     const { events: eventsData } = this.state;
-    const style = {
+    const styles = {
       overflow: 'auto',
       backgroundColor: '#fff',
       height: '100vh',
       padding: '15px',
     };
     return (
-      <div style={style}>
-        <DragAndDropCalendar
-          selectable
-          localizer={localizer}
-          events={eventsData}
-          onEventDrop={this.moveEvent}
-          resizable
-          resources={resourceMap}
-          resourceIdAccessor="resourceId"
-          resourceTitleAccessor="resourceTitle"
-          onEventResize={this.resizeEvent}
-          defaultView="day"
-          views={['day']}
-          defaultDate={new Date(2018, 0, 29)}
-          popup
-          step={30}
-          timeslots={1}
-          components={{
-            event: EventComponent,
-          }}
-          onSelectEvent={event => console.log(event.title)}
-          onSelectSlot={this.handleSelect}
-        />
+      <div style={styles}>
+        <Row gutter={15}>
+          <Col span={18}>
+            <DragAndDropCalendar
+              selectable
+              localizer={localizer}
+              events={eventsData}
+              onEventDrop={this.moveEvent}
+              resizable
+              resources={resourceMap}
+              resourceIdAccessor="resourceId"
+              resourceTitleAccessor="resourceTitle"
+              onEventResize={this.resizeEvent}
+              defaultView="day"
+              views={['day']}
+              defaultDate={new Date(2018, 0, 29)}
+              popup
+              step={30}
+              timeslots={1}
+              components={{
+                event: EventComponent,
+              }}
+              onSelectEvent={event => console.log(event.title)}
+              onSelectSlot={this.handleSelect}
+              onD
+              onDropFromOutside={this.onDropFromOutside}
+              onDragOver={this.customOnDragOver}
+            />
+          </Col>
+          <Col span={6}>
+            <Row>
+              <Calendar
+                className={style.calendar}
+                fullscreen={false}
+                mode="month"
+                onPanelChange={this.onPanelChange}
+              />
+            </Row>
+            <Row>
+              <Card>
+                {Object.entries(outData).map(([index, item]) => (
+                  <div
+                    style={{
+                      border: '2px solid gray',
+                      borderRadius: '4px',
+                      width: '100px',
+                      margin: '10px',
+                    }}
+                    draggable="true"
+                    key={index}
+                    onDragStart={() => this.handleDragStart(index, item)}
+                  >
+                    {item.name}
+                  </div>
+                ))}
+                <div
+                  style={{
+                    border: '2px solid gray',
+                    borderRadius: '4px',
+                    width: '100px',
+                    margin: '10px',
+                  }}
+                  draggable="true"
+                  key="undroppable"
+                  onDragStart={() => this.handleDragStart('undroppable')}
+                >
+                  Draggable but not for calendar.
+                </div>
+              </Card>
+            </Row>
+          </Col>
+        </Row>
       </div>
     );
   }
