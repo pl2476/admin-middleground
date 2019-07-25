e1067c9799d377912e702894c62c86b855c741b5
Light <pl2476@126.com>
2019-07-25 20:45:04 +0800

    Grid: update layout and table

--- src/pages/Grid/grid.js
+++ src/pages/Grid/grid.js
@@ -3,7 +3,43 @@ import style from './Grid.less';
 
 class Grid extends PureComponent {
   render() {
-    return <div className={style.grid} />;
+    const tableHeadList = [1, 2, 3, 4, 5, 6, 7];
+    const tableBodyList = [];
+    for (let i = 0; i < 30; i += 1) {
+      tableBodyList.push(i);
+    }
+    const colTitle = ['', ...tableHeadList].map((item, index) => (
+      <div key={item} className={style.cell}>
+        {index > 0 ? item : ''}
+      </div>
+    ));
+    const body = tableBodyList.map((rowItem, i) => (
+      <div key={rowItem} className={style.row}>
+        {['', ...tableHeadList].map((colItem, j) => (
+          <div key={colItem} className={style.cell}>
+            {j === 0 ? i + 1 : ''}
+          </div>
+        ))}
+      </div>
+    ));
+    return (
+      <div className={style.grid}>
+        <div className={style.left}>
+          <div className={style.top}>top</div>
+          <div className={style.middle}>
+            <div className={style.head}>
+              <div className={style.row}>{colTitle}</div>
+            </div>
+            <div className={style.body}>{body}</div>
+          </div>
+          <div className={style.bottom}>bottom</div>
+        </div>
+        <div className={style.right}>
+          <div className={style.top}>top</div>
+          <div className={style.bottom}>bottom</div>
+        </div>
+      </div>
+    );
   }
 }
 
